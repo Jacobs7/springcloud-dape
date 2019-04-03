b532c829691390a7c98c24a02e7184df228dfbab
DP <xushifu@live.cn>
2019-04-03 15:13:07 +0800

    First Commit 1 Eureka 2 Config / ConfigClient

--- datatech-config/src/main/java/com/datatech/cloud/datatechconfig/DatatechConfigApplication.java
+++ datatech-config/src/main/java/com/datatech/cloud/datatechconfig/DatatechConfigApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
-@EnableConfigServer
-@SpringBootApplication
-@RefreshScope
+@SpringBootApplication    //Boot项目
+@EnableEurekaClient       //服务发现
+@EnableConfigServer       //配置中心服务
 public class DatatechConfigApplication {
 
     public static void main(String[] args) {
